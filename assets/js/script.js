8c43dc983b35430d67324f1a58559de7373334bc
sidneylino <sidneymartinslino@gmail.com>
2025-02-21 18:34:47 -0300

    adding_certificates_section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
 // show and hide navbar on mobile
-function toggleMenu() {
+toggleMenu = () => {
     const navLinks = document.querySelector('.navbar__links');
     navLinks.classList.toggle('show');
+}
+
+changeCertificates = (certificate) => {            
+    const allCertificates = document.querySelectorAll(".container__certificate");
+    allCertificates.forEach(el => {
+        el.classList.remove("container__certificate--active");
+    });
+
+    switch (certificate) {
+        case 'bradesco':
+            document.querySelector(".container__certificate--one").classList.add("container__certificate--active");
+            break;
+
+        case 'pythonando':
+            document.querySelector(".container__certificate--six").classList.add("container__certificate--active");
+            break;      
+
+        case 'hashtag':
+            document.querySelector(".container__certificate--four").classList.add("container__certificate--active");
+            break;      
+
+        case 'empower':
+            document.querySelector(".container__certificate--three").classList.add("container__certificate--active");
+            break;      
+
+        case 'rocket':
+            document.querySelector(".container__certificate--two").classList.add("container__certificate--active");
+            break;
+        
+        default:
+            document.querySelector(".container__certificate--one").classList.add("container__certificate--active");
+    }
 }
