9e8a03ed6da7aca7791cc2b38918a44426368520
sidneylino <sidneymartinslino@gmail.com>
2025-02-21 21:57:23 -0300

    certificates_section_update

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,28 +10,39 @@ changeCertificates = (certificate) => {
         el.classList.remove("container__certificate--active");
     });
 
+    const allCertificatesIcons = document.querySelectorAll(".container__icon");
+    allCertificatesIcons.forEach(el => {
+        el.classList.remove("container__background");
+    });
+
     switch (certificate) {
         case 'bradesco':
             document.querySelector(".container__certificate--one").classList.add("container__certificate--active");
+            document.querySelector(".container__icon--one").classList.add("container__background");
             break;
 
         case 'pythonando':
             document.querySelector(".container__certificate--six").classList.add("container__certificate--active");
+            document.querySelector(".container__icon--two").classList.add("container__background");
             break;      
 
         case 'hashtag':
             document.querySelector(".container__certificate--four").classList.add("container__certificate--active");
+            document.querySelector(".container__icon--five").classList.add("container__background");
             break;      
 
         case 'empower':
             document.querySelector(".container__certificate--three").classList.add("container__certificate--active");
+            document.querySelector(".container__icon--three").classList.add("container__background");
             break;      
 
         case 'rocket':
             document.querySelector(".container__certificate--two").classList.add("container__certificate--active");
+            document.querySelector(".container__icon--four").classList.add("container__background");
             break;
         
         default:
-            document.querySelector(".container__certificate--one").classList.add("container__certificate--active");
+            document.querySelector(".container__certificate--four").classList.add("container__certificate--active");
+            document.querySelector(".container__icon--four").classList.add("container__background");
     }
 }
